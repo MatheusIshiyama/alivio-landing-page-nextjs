4d137494f0c9f04b6631a50b91897957fdc1d758
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-30 18:18:35 -0300

    create header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import styles from "../css/Home.module.css";
 
 export default function Home() {
     return (
         <>
-        <div>Alivio Landing Page</div>
+            <header>
+                <a href="#">
+                    <img src="./AlivioLogo.png"></img>{" "}
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">Why Alivio</a>
+                    </li>
+                    <li>
+                        <a href="#">Solutions</a>
+                    </li>
+                    <li>
+                        <a href="#">Community</a>
+                    </li>
+                    <li>
+                        <a href="#">Pricing</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">Sign in</a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.trialButton}>
+                            <label>Start trial</label>
+                        </a>
+                    </li>
+                </ul>
+            </header>
         </>
-    )
+    );
 }
