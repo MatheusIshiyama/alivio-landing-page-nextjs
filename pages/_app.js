d29c093bff3fb4c96e04695198ee8de468e68ac5
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-30 17:31:00 -0300

    adding title and set viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
 import "../css/styles.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Alivio</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
